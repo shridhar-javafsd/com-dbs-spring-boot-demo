1dab1d188e5835eec6e526256608394d0b2c432d
vamandeshmukh <vamandeshmukh@gmail.com>
2021-11-18 10:21:39 +0530

    n

--- src/main/java/com/dbs/demo/controller/EmployeeController.java
+++ src/main/java/com/dbs/demo/controller/EmployeeController.java
@@ -2,6 +2,7 @@ package com.dbs.demo.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,12 +14,14 @@ public class EmployeeController {
 
 	private static final Logger LOG = LoggerFactory.getLogger(EmployeeController.class);
 
+	@Autowired
 	EmployeeService empService;
 
 //	http://localhost:8082/emp
 	@GetMapping("/emp")
-	public Employee getEmp(int eid) {
+	public Employee getEmp() {
 		LOG.info("getEmp");
+		int eid = 10;
 		return empService.getEmployeeById(eid);
 	}
 }
