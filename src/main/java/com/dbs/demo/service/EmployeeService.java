1844abfcb1b2258cd3f7367bf32e911f59dd9287
vamandeshmukh <vamandeshmukh@gmail.com>
2021-11-18 15:06:24 +0530

    Update EmployeeService.java

--- src/main/java/com/dbs/demo/service/EmployeeService.java
+++ src/main/java/com/dbs/demo/service/EmployeeService.java
@@ -2,22 +2,27 @@ package com.dbs.demo.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.dbs.demo.model.Employee;
+import com.dbs.demo.repository.EmployeeRepository;
 
 @Service
 public class EmployeeService {
+
+	@Autowired
+	EmployeeRepository empRepository;
 
 	private static final Logger LOG = LoggerFactory.getLogger(EmployeeService.class);
 
 	public Employee getEmployeeById(int eid) {
 		LOG.info("getEmployeeById");
-		return new Employee(eid, "Sonu", 56000);
+		return empRepository.findById(eid).get();
 	}
 
 	public Employee addEmployee(Employee employee) {
 		LOG.info("addEmployee");
-		return employee;
+		return empRepository.save(employee); // insert
 	}
 }
