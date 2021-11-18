ec0e3013f6f731e5b090461be7c23956684df722
vamandeshmukh <vamandeshmukh@gmail.com>
2021-11-18 14:51:48 +0530

    Update EmployeeRepository.java

--- src/main/java/com/dbs/demo/repository/EmployeeRepository.java
+++ src/main/java/com/dbs/demo/repository/EmployeeRepository.java
@@ -1,5 +1,11 @@
 package com.dbs.demo.repository;
 
-public interface EmployeeRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.dbs.demo.model.Employee;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
