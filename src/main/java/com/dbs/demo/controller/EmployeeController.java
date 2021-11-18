e055b39fa3f24ce54a6ad209ec4ef7a52bf2d78b
vamandeshmukh <vamandeshmukh@gmail.com>
2021-11-18 11:39:09 +0530

    Update EmployeeController.java

--- src/main/java/com/dbs/demo/controller/EmployeeController.java
+++ src/main/java/com/dbs/demo/controller/EmployeeController.java
@@ -5,6 +5,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,5 +31,12 @@ public class EmployeeController {
 
 //	@GetMapping("/getemp/{did}/{eid}")
 //	public Employee getEmpById(@PathVariable(name = "eid") int eid, @PathVariable(name = "did") int did) {
+
+//	http://localhost:8082/emp/addemp 
+	@PostMapping("/addemp")
+	public Employee addEmp(@RequestBody Employee employee) {
+		LOG.info("addEmp");
+		return empService.addEmployee(employee);
+	}
 
 }
