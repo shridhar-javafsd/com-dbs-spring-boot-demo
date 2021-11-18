1266bd5abe90d5391fec84cdd42e18cec34886f1
vamandeshmukh <vamandeshmukh@gmail.com>
2021-11-18 09:29:28 +0530

    Commit

--- src/main/java/com/dbs/demo/App.java
+++ src/main/java/com/dbs/demo/App.java
@@ -2,15 +2,12 @@ package com.dbs.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.context.annotation.ComponentScan;
-
-//@ComponentScan 
 
 @SpringBootApplication
-public class ComDbsSpringBootDemoApplication {
+public class App {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ComDbsSpringBootDemoApplication.class, args);
+		SpringApplication.run(App.class, args);
 	}
 
 }
