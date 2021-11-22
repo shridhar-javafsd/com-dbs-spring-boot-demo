dba7ef9efd6fab9c89101767261318d38d2e8963
vamandeshmukh <vamandeshmukh@gmail.com>
2021-11-22 11:34:46 +0530

    C

--- src/main/java/com/dbs/demo/model/Employee.java
+++ src/main/java/com/dbs/demo/model/Employee.java
@@ -2,14 +2,7 @@ package com.dbs.demo.model;
 
 import java.io.Serializable;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "emp_table")
